0e1ce114961fcb6dfc05e520d5581a3ac6467967
jankowskipawel <39927846+jankowskipawel@users.noreply.github.com>
2021-06-06 16:53:46 +0200

    Add face recognition in image

--- face-tracking/face-tracking/Form1.Designer.cs
+++ face-tracking/face-tracking/Form1.Designer.cs
@@ -46,6 +46,9 @@ namespace face_tracking
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.Location = new System.Drawing.Point(12, 41);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(848, 574);
--- face-tracking/face-tracking/Form1.cs
+++ face-tracking/face-tracking/Form1.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
 
 namespace face_tracking
 {
@@ -16,12 +20,47 @@ namespace face_tracking
         public Form1()
         {
             InitializeComponent();
+            img = ResizeImage(img, pictureBox1.Size);
+            pictureBox1.Image = DetectFaces(img);
         }
 
+        //initialize classifier
+        CascadeClassifier faceCascadeClassifier = new CascadeClassifier("haarcascade_frontalface_alt.xml");
+
+        //initialize web client for downloading images
+        private static WebClient wc = new WebClient();
+        private static byte[] bytes = wc.DownloadData("https://thispersondoesnotexist.com/image");
+        private static MemoryStream ms = new MemoryStream(bytes);
+        private System.Drawing.Image img = System.Drawing.Image.FromStream(ms);
 
         private void getNewImageButton_Click(object sender, EventArgs e)
         {
+            //download new image
+            bytes = wc.DownloadData("https://thispersondoesnotexist.com/image");
+            ms = new MemoryStream(bytes);
+            img = System.Drawing.Image.FromStream(ms);
+            //resize and replace
+            img = ResizeImage(img, pictureBox1.Size);
+            pictureBox1.Image = DetectFaces(img);
+        }
 
+        private Image DetectFaces(Image img)
+        {
+            Bitmap bitmap = new Bitmap(img);
+            Image<Bgr, byte> grayImage = new Image<Bgr, byte>(bitmap);
+            //detect faces
+            Rectangle[] faces = faceCascadeClassifier.DetectMultiScale(grayImage, 1.1, 5);
+            //draw rectangles where faces were detected
+            foreach (var face in faces)
+            {
+                CvInvoke.Rectangle(grayImage, face, new Bgr(Color.Red).MCvScalar, 2);
+            }
+            return grayImage.ToBitmap();
+        }
+
+        public static Image ResizeImage(Image imgToResize, Size size)
+        {
+            return (Image)(new Bitmap(imgToResize, size));
         }
     }
 }
