9a8e562a5b2917589ec930d50146ddc871394d01
jankowskipawel <39927846+jankowskipawel@users.noreply.github.com>
2021-06-22 15:34:29 +0200

    Add log in  option

---
+++ ListsWebApp/ListsWebApp/Data/LoginState.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListsWebApp.Data
+{
+    public class LoginState
+    {
+        public bool IsLoggedIn = false;
+        public string username { get; set; }
+        public int userId = -1;
+        public event Action OnChange;
+
+        public void SetLogin(bool login, string userName, int id)
+        {
+            IsLoggedIn = login;
+            username = userName;
+            userId = id;
+            NotifyStateChanged();
+        }
+
+        private void NotifyStateChanged()
+        {
+            OnChange?.Invoke();
+        }
+    }
+}
--- ListsWebApp/ListsWebApp/Startup.cs
+++ ListsWebApp/ListsWebApp/Startup.cs
@@ -31,6 +31,7 @@ namespace ListsWebApp
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
             services.AddSingleton<IDataAccess, DataAccess>();
+            services.AddScoped<LoginState>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
