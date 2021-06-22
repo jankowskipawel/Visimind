f7c56f276b9004aae252c1ec7eb9aa58602e779d
jankowskipawel <39927846+jankowskipawel@users.noreply.github.com>
2021-06-22 16:57:47 +0200

    Add lists sharing

---
+++ ListsWebApp/ListsWebApp/Models/SharedList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListsWebApp.Models
+{
+    public class SharedList
+    {
+        public int UserId { get; set; }
+        public int ListId { get; set; }
+        public string Id { get; set; }
+    }
+}
