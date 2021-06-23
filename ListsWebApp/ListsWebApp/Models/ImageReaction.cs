667c48001bad3dd7f64098956a38f14a76af5b7c
jankowskipawel <39927846+jankowskipawel@users.noreply.github.com>
2021-06-23 15:37:57 +0200

    Add image reactions

---
+++ ListsWebApp/ListsWebApp/Models/ImageReaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListsWebApp.Models
+{
+    public class ImageReaction
+    {
+        public int Id { get; set; }
+        public int GivenByUserId { get; set; }
+        public int ListItemId { get; set; }
+        public bool IsPositive { get; set; }
+    }
+}
